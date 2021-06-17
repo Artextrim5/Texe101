2f3a0e71f583863dc87207ab28651bf239368f42
Artextrim5@gmail.com <Artextrim5@gmail.com>
2021-06-17 21:11:55 +0300

    оппа

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;                 // добавляем поле в которое можно вставить текст
+    [SerializeField] State startingState;                // добавляем поле в которое можно вставить переменную State
+
+    State state;                                         // создаем переменную state тип State
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;                          // при страрте присваеваем нашей переменной значение стартовой переменной
+        textComponent.text = state.GetStateStory();     // считываем текст из нашей переменной
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+        ExitButton();
+    }
+    private void ExitButton()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+    private void ManageState()
+    {
+
+        var nextStates = state.GetNextStates();         // отслеживаем текущий стайт
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+
+/*        if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    state = nextStates[0];
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    state = nextStates[1];
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    state = nextStates[2];
+                }
+        */
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]                     // создаем новый тип переменной
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText; // создаем поле для ввода текста в нашу новую переменную
+    [SerializeField] State[] nextStates;                  // объявляем массив с возможностью заносить в него элементы
+    public string GetStateStory()
+    {
+        return storyText;                                 // возвращаем по завершению метода текст, который был записан в текстовое поле
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+}
